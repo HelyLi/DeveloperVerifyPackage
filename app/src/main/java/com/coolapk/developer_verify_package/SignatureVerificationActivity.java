75b177bafa5ac35a7b60e5d756878651bc22dd23
HelyLi <1772063751@qq.com>
2019-06-16 14:40:05 +0800

    add src

---
+++ app/src/main/java/com/coolapk/developer_verify_package/SignatureVerificationActivity.java
@@ -0,0 +1,13 @@
+package com.coolapk.developer_verify_package;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignatureVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signature_verification);
+    }
+}
